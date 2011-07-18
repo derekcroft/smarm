a79c4b0b2acf1515251988bd52a69a87a3998466
Derek Croft <derek@derekcroft.com>
2011-07-18 18:17:19 -0400

    First commit

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '97b6c107d7c1ca2cb88f71d16ac96b82dfee27303e49c09c3c894349338c44393c4070fdf4cdb097d399d6c0a6fbe66b5cb1c10990c7c7b7e152545958187d97'
---
+++ spec/smarm_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Smarm do
+  it "should be valid" do
+    Smarm.should be_a(Module)
+  end
+end
